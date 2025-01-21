98fa6e1b613e0ca980af5d004edbfb234cba07bc
teekimo <teekimo@outlook.com>
2025-01-21 19:58:37 +0800

    :new: add 'at' user show nickname

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,12 @@ import { Quester, Schema } from "koishi"
 
   export type Config = {
     path: string
-    autoAters: boolean
     consoleLog: boolean
   }
 
   export const Config: Schema<Config> = Schema.intersect([
     Schema.object({
       path: Schema.string().description("wcferry 通知监听路径").default('/wechatFerry'),
-      autoAters: Schema.boolean().description("是否自动艾特发出指令的人员(群聊)").default(false),
       consoleLog: Schema.boolean().description("是否在控制台输出接收信息日志").default(false),
     }),
     Quester.createConfig("http://127.0.0.1:10010")
--- src/message.ts
+++ src/message.ts
@@ -1,12 +1,26 @@
-import { Dict, Element, MessageEncoder } from "koishi";
+import { Context, Dict, Element, MessageEncoder } from "koishi";
+import { WcFerryBot } from "./bot";
+import { Config } from "./config";
 
-export class WcFerryMessageEncoder extends MessageEncoder {
+export class WcFerryMessageEncoder<C extends Context> extends MessageEncoder<C, WcFerryBot<C, Config>> {
   private payload: Dict;
 
   // 发送缓冲区内的消息
   async flush(): Promise<void> {
     if (this.payload.text) {
       this.payload.text = this.payload.text.trimStart().trimEnd();
+      if (this.payload.at) {
+        const {data: atUser} = await this.bot.internal.getDBContactInfo(this.payload.at);
+        this.payload.text = `@${atUser?.NickName} ${this.payload.text}`;
+      }
+    }
+
+    if (this.payload.img) {
+      await this.bot.internal.sendImage(this.payload.img, this.channelId)
+    }
+
+    if (this.payload.file) {
+      await this.bot.internal.sendImage(this.payload.file, this.channelId)
     }
   }
 
@@ -28,6 +42,10 @@ export class WcFerryMessageEncoder extends MessageEncoder {
 
     if (type === "img" || type === "image") {
       this.payload.img = attrs.src;
+    }
+
+    if (type === 'at') {
+      this.payload.at = attrs.id;
     }
 
     if (type === "p") {
